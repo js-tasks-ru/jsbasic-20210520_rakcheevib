043ca07d53ae57dfdb343f3710a4c3f004ad03e8
Ivan Rakcheev <rakcheevib@alfastrah.ru>
2021-06-01 19:18:42 +0300

    выполнены задачи для 3 занятия

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,19 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(/[\s,]+/);
+let newArr = [];
+for(let i = 0; i < arr.length; i++)
+{
+    arr[i] = arr[i].replace(',', '');
+}
+for( let i of arr){
+if(!isNaN(parseFloat(+i))){
+newArr.push(i);
+}
+}
+let res = {};
+let max = Math.max.apply( null, newArr );
+let min = Math.min.apply( null, newArr );
+res.max = max;
+res.min = min;
+return res;
 }
