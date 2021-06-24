35caba0315e48c886c49b49656841ae495dc7b94
Ivan Rakcheev <rakcheevib@alfastrah.ru>
2021-06-24 18:26:42 +0300

    выполнены задачи для 6 занятия

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,35 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.data = rows;
+    this.elem = document.createElement('table');
+
+    for(const row of this.data){
+      this.name = row.name;
+      this.age = row.age;
+      this.salary = row.salary;
+      this.city = row.city;
+      this.item =  '<tr>' + 
+      '<td>' + this.name + '</td>' + 
+      '<td>' + this.age + '</td>' + 
+      '<td>' + this.salary + '</td>' + 
+      '<td>' + this.city + '</td>' + 
+      '<td>' + '<button>' + 'X' + '</button>' + '</td>' + 
+      '</tr>';
+
+      this.elem.insertAdjacentHTML('beforeEnd', this.item);
+    }
+    this.elem.addEventListener('click', this.onClickRow);
   }
+  
+  render(){
+      return this.elem;
+  }
+
+  onClickRow(event){
+    if(event.target.tagName == 'BUTTON'){
+      event.target.closest('tr').remove();
+    }
+  }
+
 }
